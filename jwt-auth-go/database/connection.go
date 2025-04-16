6eb458f910851f045bd97ae935a51dd34cbfd7d5
Malik Saddique <maliksaddique139@gmail.com>
2025-04-16 10:27:20 +0500

    WIP: db connection

---
+++ jwt-auth-go/database/connection.go
@@ -0,0 +1,45 @@
+package connection
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+
+	_ "github.com/lib/pq"
+)
+
+func DbConnection() {
+
+	host := os.Getenv("DB_HOST")
+	portStr := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		fmt.Println("Error converting port to integer:", err)
+		return
+	}
+
+	psqlconn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		fmt.Println("Error opening DB connection:", err)
+		return
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Failed to connect to database:", err)
+		return
+	}
+
+	fmt.Println("Connected to PostgreSQL successfully!")
+}
